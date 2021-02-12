87b92760f426183400c7eeb4015a4d01873917dc
TristaWu <trista@kcdgs.com>
2021-02-12 21:08:28 +0800

    Initial Commit

---
+++ ImageUIGestureRecognizerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageUIGestureRecognizerDemo
+//
+//  Created by Trista on 2021/2/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
